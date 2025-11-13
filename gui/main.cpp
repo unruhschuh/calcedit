9a842ecb1c7daf4b2b79234d77e88555ccc7791c
Thomas Leitz <thomas.leitz@web.de>
2025-11-13 18:07:36 +0100

    fixed windows build

--- gui/main.cpp
+++ gui/main.cpp
@@ -4,7 +4,9 @@
 
 int main(int argc, char *argv[])
 {
+  qputenv("QT_QPA_PLATFORM", "windows:darkmode=0");
   QApplication a(argc, argv);
+  a.setStyle("windowsvista");
   MainWindow w;
   w.show();
   return a.exec();
